df0cb7fa6e3020663f3b43e392a5085a930f0f95
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-07-30 15:19:08 +0200

    maj

--- controllers/agencyControllers.js
+++ controllers/agencyControllers.js
@@ -1,4 +1,6 @@
+import express from 'express'
 import AgencyModel from '../models/agencyModel.js'
+const app = express();
 
 
 export const addAgency = async function(req, res) {
@@ -27,4 +29,49 @@ export const deleteAgency = async function(req,res) {
         res.status(404).send('Aucun fichier trouvé.')
     }
     res.status(200).send()
+}
+
+// ** Création du token d'authentification avec jwt
+app.post('/api/login', async (req, res) => { // route d'authentification
+    // const bcrypt = require('bcryptjs') // cryptage du mdp avec bcrypt
+    // let salt = await bcrypt.genSalt(10)
+    // let hash = await bcrypt.hash(req.password, salt)
+
+    console.log(req.body.pseudo);
+
+    const agency = await Agency.login(req.body.pseudo, req.body.password); // login de l'utilisateur avec pseudo et mot de passe
+    console.log(agency)
+    if (agency) {
+        const token = jwt.sign({agency}, 'my_secret_key'); // génération du token
+        res.json({
+            token, agency
+        });
+    } else {
+        res.status(404).send('Utilisateur inexistant.')
+    }
+});
+
+app.get('/api/protected', ensureToken, (req, res) => {
+    jwt.verify(req.token, 'my_secret_key', (err, data) => {
+        if (err) {
+            res.sendStatus(403); // si erreur, va envoyer un statut erreur ou que son token n'existe pas
+        } else {
+            res.json({
+                text: 'protected',
+                data: data
+            });
+        }
+    })
+});
+function ensureToken(req, res, next) { // Fonction qui sert à vérifier que l'user qui suit cette route a créé un token avant
+    const bearerHeader = req.headers['authorization'];
+    console.log(bearerHeader);
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1]; // Bearer = prefixe token
+        req.token = bearerToken; // conserve le token dans l'objet de la demande
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 }
--- index.js
+++ index.js
@@ -24,50 +24,8 @@ app.listen(PORT, () => {
     console.log(`Serveur lancé sur le port : ${PORT}`) // Permet de savoir si le serveur est bien relancé
 })
 
-// ------------------------------------------------------------------------------------------------------- //
 
-// ** Création du token d'authentification avec jwt
-app.post('/api/login', async (req, res) => { // route d'authentification
-    // const bcrypt = require('bcryptjs') // cryptage du mdp avec bcrypt
-    // let salt = await bcrypt.genSalt(10)
-    // let hash = await bcrypt.hash(req.password, salt)
 
-    console.log(req.body.pseudo);
 
-    const agency = await Agency.login(req.body.pseudo, req.body.password); // login de l'utilisateur avec pseudo et mot de passe
-    console.log(agency)
-    if (agency) {
-        const token = jwt.sign({agency}, 'my_secret_key'); // génération du token
-        res.json({
-            token, agency
-        });
-    } else {
-        res.status(404).send('Utilisateur inexistant.')
-    }
-});
 
-app.get('/api/protected', ensureToken, (req, res) => {
-    jwt.verify(req.token, 'my_secret_key', (err, data) => {
-        if (err) {
-            res.sendStatus(403); // si erreur, va envoyer un statut erreur ou que son token n'existe pas
-        } else {
-            res.json({
-                text: 'protected',
-                data: data
-            });
-        }
-    })
-});
 
-function ensureToken(req, res, next) { // Fonction qui sert à vérifier que l'user qui suit cette route a créé un token avant
-    const bearerHeader = req.headers['authorization'];
-    console.log(bearerHeader);
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1]; // Bearer = prefixe token
-        req.token = bearerToken; // conserve le token dans l'objet de la demande
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
