dddc4b63dea21ece0a495720b2a81f6b548d796f
laurieftn <laurie.ftn@gmail.com>
2021-08-23 13:54:57 +0200

    rm node_modules

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ import mongoose from 'mongoose'
 import routes from './routes/routes.js'
 import dotenv from 'dotenv'
 dotenv.config()
-<<<<<<< HEAD
 import jwt from 'jsonwebtoken'
 import Agency from './models/agencyModel.js'
-=======
->>>>>>> 8f82f14707d84c0e0ea4c09822dbc25b971cae62
 
 const PORT = process.env.PORT || 3000; // Déclare le port utilisé
-
 const app = express(); // création de l'objet représentant l'application express
 
 app.use(express.json()) // pour que le serveur retourne les données en json
