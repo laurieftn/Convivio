a7481ab00e6e6853c5a5c939d069cb13088b4738
laurieftn <laurie.ftn@gmail.com>
2021-08-27 15:17:19 +0200

    màj

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,12 @@
 import express from 'express'
-<<<<<<< HEAD
-import {addEvent , getEvents , getOneEvent , getCustomerEvents, updateEvent ,deleteEvent}from '../controllers/eventControllers.js'
-import { addRequest, getRequests, getRequest, deleteRequest } from '../controllers/requestControllers.js'
-import { addAgency, getAgency, updateAgency, deleteAgency, auth, protectedLaurie, ensureToken } from '../controllers/agencyControllers.js'
-=======
 import { createEvent, deleteEvent, updateEvent, getEvent, getAllEvents, getAllEventsFromCustomer, getAllEventsFromDate, getAllEventsFromCity, getAllEventsFromProvider }from '../controllers/eventControllers.js'
 import { createEquipment, deleteEquipment, updateEquipment, getEquipment, getAllEquipments, getAllEquipmentsByType } from '../controllers/stockControllers.js'
 import { createUser, updateUser, deleteUser, getUser, getAllUsers, login, ensureToken } from '../controllers/userControllers.js'
 import { createServiceProvider, deleteServiceProvider, updateServiceProvider, getServiceProvider, getAllServiceProviders, getAllServiceProvidersByType } from '../controllers/serviceProvidersControllers.js'
->>>>>>> laurie
 import { catchErrors } from './../helpers.js'
 
 const router = express.Router()
 
-<<<<<<< HEAD
-//test
-// ----------------------------------
-router.get('/event/:name', catchErrors(getCustomerEvents)) // Visualisation de tous les events du client
-// Login routes
-router.post('/api/login', (auth))
-router.use(ensureToken); // Toutes les routes sous le ensureToken sont protégées
-router.get('/api/protected', (protectedLaurie))
-=======
 // LOGIN + PROTECTION des routes
 router.post('/api/login', (login))
 // router.use(ensureToken); // Toutes les routes sous le ensureToken sont protégées
@@ -47,7 +32,6 @@ router.get('/getAllUsers', catchErrors(getAllUsers))
 router.patch('/updateUser/:id', catchErrors(updateUser))
 router.delete('/deleteUser/:id', catchErrors(deleteUser))
 
->>>>>>> laurie
 // ----------------------------------
 // STOCK --- Routes liées à la gestion des stocks des équipements
 router.post('/createEquipment', createEquipment)
@@ -58,29 +42,6 @@ router.delete('/deleteEquipment/:id', catchErrors(deleteEquipment))
 router.patch('/updateEquipment/:id', catchErrors(updateEquipment))
 
 // ----------------------------------
-<<<<<<< HEAD
-
-// Routes liées aux évènements
-router.post('/event', catchErrors(addEvent)) // Créer un event 
-router.get('/listOfEvents', catchErrors(getEvents)) // Liste de tous les events 
-router.get('/event/:id', catchErrors(getOneEvent)) // Visualisation d'un event
-// router.get('/event/:name', catchErrors(getCustomerEvents)) // Visualisation de tous les events du client
-router.patch('/event/:id', catchErrors(updateEvent)) // Mise à jour d'un event 
-router.delete('/event/:id', catchErrors(deleteEvent)) // Suppression d'un event 
-// ----------------------------------
-
-// Routes liées aux utilisateurs de l'agence
-router.post('/agency', catchErrors(addAgency))
-router.get('/agency/:id', catchErrors(getAgency))
-router.patch('/agency/:id', catchErrors(updateAgency))
-router.delete('/agency/:id', catchErrors(deleteAgency))
-// ----------------------------------
-
-export default router
-
-
-
-=======
 // SERVICE PROVIDERS --- Routes liées aux prestataires
 router.post('/createServiceProvider', catchErrors(createServiceProvider)) // Créer un prestataire
 router.get('/getAllServiceProviders', catchErrors(getAllServiceProviders)) // Liste de tous les prestataires
@@ -90,4 +51,3 @@ router.patch('/updateServiceProvider/:id', catchErrors(updateServiceProvider)) /
 router.delete('/deleteServiceProvider/:id', catchErrors(deleteServiceProvider)) // Suppression d'un prestataire
 
 export default router
->>>>>>> laurie
